9b308efe0ff4062e0764abc4feced045116122da
Talitha <talkruger9@gmail.com>
2020-01-21 11:38:18 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+
+
+ const Welcome = () => (
+        <div>
+            <h1>Welcome to the Experimint!</h1>
+                <p>Yes the spelling error is intentional</p>
+        </div>
+)
+
+export default Welcome;
